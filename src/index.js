d51ba70bbdcc8be72f26cb4491830a8c2fe5cbaf
Scott Thiessen <scott@varyd.io>
2017-08-17 15:08:28 -0700

    Export classes differently than utils

--- src/index.js
+++ src/index.js
@@ -5,16 +5,12 @@ import * as maths from "./maths";
 import * as random from "./random";
 import * as text from "./text";
 
-import * as Rect from "./Rect";
-import * as Range from "./Range";
-import * as Vec from "./Vec";
-
 export { arrays };
 export { geom };
 export { maths };
 export { random };
 export { text };
 
-export { Rect };
-export { Range };
-export { Vec };
+export { Rect } from "./Rect";
+export { Range } from "./Range";
+export { Vec } from "./Vec";
