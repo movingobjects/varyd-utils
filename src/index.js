1f847d956456675f69f060d0c89f7c0dc91c8be1
Scott Thiessen <scott@varyd.io>
2017-08-17 15:11:30 -0700

    Export with default for data types

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ export { maths };
 export { random };
 export { text };
 
-export { Rect } from "./Rect";
-export { Range } from "./Range";
-export { Vec } from "./Vec";
+export { default as Rect } from "./Rect";
+export { default as Range } from "./Range";
+export { default as Vec } from "./Vec";
