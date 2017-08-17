1470b481620c51d26ecddf44940684a6611c70e8
Scott Thiessen <scott@varyd.io>
2017-08-17 15:03:38 -0700

    Undoing index change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 
-export { arrays } from "./arrays";
-export { geom } from "./geom";
-export { maths } from "./maths";
-export { random } from "./random";
-export { text } from "./text";
+import * as arrays from "./arrays";
+import * as geom from "./geom";
+import * as maths from "./maths";
+import * as random from "./random";
+import * as text from "./text";
 
-export { Rect } from "./Rect";
-export { Range } from "./Range";
-export { Vec } from "./Vec";
+import * as Rect from "./Rect";
+import * as Range from "./Range";
+import * as Vec from "./Vec";
+
+export { arrays };
+export { geom };
+export { maths };
+export { random };
+export { text };
+
+export { Rect };
+export { Range };
+export { Vec };
